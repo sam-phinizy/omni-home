0d5e52bf0130719ebff537f8f5dc3f525e19751d
Sam Phinizy <nipper@dorkery.com>
2024-12-07 13:25:22 -0500

    Adds in a created by and updated by for inventory

--- inventory/admin.py
+++ inventory/admin.py
@@ -4,7 +4,13 @@ from .models import Inventory
 
 @admin.register(Inventory)
 class InventoryAdmin(admin.ModelAdmin):
-    list_display = ["name", "location", "quantity", "created_at", "updated_at"]
-    list_filter = ["location", "created_at", "updated_at"]
-    search_fields = ["name", "description", "location__name"]
-    readonly_fields = ["created_at", "updated_at"]
+    list_display = ["name", "location", "quantity", "created_by", "created_at", "updated_by", "updated_at"]
+    list_filter = ["location", "created_by", "updated_by", "created_at", "updated_at"]
+    search_fields = ["name", "description", "location__name", "created_by__email", "updated_by__email"]
+    readonly_fields = ["created_at", "updated_at", "created_by", "updated_by"]
+
+    def save_model(self, request, obj, form, change):
+        if not change:  # If creating a new object
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Inventory(models.Model):
@@ -12,6 +13,18 @@ class Inventory(models.Model):
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='created_items'
+    )
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='updated_items'
+    )
 
     class Meta:
         verbose_name_plural = "Inventory"

a6694760acfa9a1f5bf3c0b12b52ad1e29e687eb
Sam Phinizy <nipper@dorkery.com>
2024-12-07 13:10:54 -0500

    Adds in tailwind

--- omni_home/settings.py
+++ omni_home/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "omni_home.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

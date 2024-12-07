74a86c34afa3304abe9d7fbdff7dd38a0caa7917
Sam Phinizy <nipper@dorkery.com>
2024-12-07 17:03:29 -0500

    Adds in an item types view on the dashboard

--- omni_home/views.py
+++ omni_home/views.py
@@ -5,8 +5,10 @@ from inventory.models import Inventory
 
 @login_required
 def dashboard(request):
-    latest_items = Inventory.objects.select_related("location", "created_by").order_by(
-        "-created_at"
-    )[:10]
+    latest_items = Inventory.objects.select_related(
+        "location", "created_by"
+    ).prefetch_related(
+        "types"
+    ).order_by("-created_at")[:10]
 
     return render(request, "dashboard.html", {"latest_items": latest_items})

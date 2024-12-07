78dc1aa370f348f61dc8da1cc2442fd173f668a8
Sam Phinizy <nipper@dorkery.com>
2024-12-07 13:44:15 -0500

    Adds in inventory types

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
-from .models import Inventory
+from .models import Inventory, InventoryType
+
+
+@admin.register(InventoryType)
+class InventoryTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "key", "created_at", "updated_at"]
+    search_fields = ["name", "key"]
+    prepopulated_fields = {"key": ("name",)}
 
 
 @admin.register(Inventory)
 class InventoryAdmin(admin.ModelAdmin):
-    list_display = ["name", "location", "quantity", "created_by", "created_at", "updated_by", "updated_at"]
-    list_filter = ["location", "created_by", "updated_by", "created_at", "updated_at"]
+    list_display = ["name", "location", "quantity", "get_types", "created_by", "created_at", "updated_by", "updated_at"]
+    list_filter = ["location", "types", "created_by", "updated_by", "created_at", "updated_at"]
     search_fields = ["name", "description", "location__name", "created_by__email", "updated_by__email"]
     readonly_fields = ["created_at", "updated_at", "created_by", "updated_by"]
+    filter_horizontal = ['types']
+
+    def get_types(self, obj):
+        return ", ".join([t.name for t in obj.types.all()])
+    get_types.short_description = "Types"
 
     def save_model(self, request, obj, form, change):
         if not change:  # If creating a new object
--- inventory/models.py
+++ inventory/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.conf import settings
 
 
+class InventoryType(models.Model):
+    name = models.CharField(max_length=255)
+    key = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
 class Inventory(models.Model):
     name = models.CharField(max_length=255)
     location = models.ForeignKey(
@@ -9,6 +22,7 @@ class Inventory(models.Model):
         on_delete=models.CASCADE,
         related_name="inventory_items"
     )
+    types = models.ManyToManyField(InventoryType, related_name="items")
     quantity = models.PositiveIntegerField()
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)

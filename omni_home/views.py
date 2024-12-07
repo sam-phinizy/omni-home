c3991690ff36a7c47dba21f9a3e9cf6af2a471e5
Sam Phinizy <nipper@dorkery.com>
2024-12-07 16:59:03 -0500

    Fixes db stuff

--- omni_home/views.py
+++ omni_home/views.py
@@ -5,10 +5,8 @@ from inventory.models import Inventory
 
 @login_required
 def dashboard(request):
-    latest_items = Inventory.objects.select_related(
-        'location', 'created_by', 'item_type'
-    ).order_by('-created_at')[:10]
-    
-    return render(request, 'dashboard.html', {
-        'latest_items': latest_items
-    }) 
+    latest_items = Inventory.objects.select_related("location", "created_by").order_by(
+        "-created_at"
+    )[:10]
+
+    return render(request, "dashboard.html", {"latest_items": latest_items})

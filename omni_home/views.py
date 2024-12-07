11a157e579c83c6e45d192d6dc896cb9b406bc06
Sam Phinizy <nipper@dorkery.com>
2024-12-07 13:30:40 -0500

    Adds in a 'parent location' view in the main dashboard

--- omni_home/views.py
+++ omni_home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from inventory.models import Inventory
 
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html') 
+    latest_items = Inventory.objects.select_related(
+        'location', 'created_by'
+    ).order_by('-created_at')[:10]
+    
+    return render(request, 'dashboard.html', {
+        'latest_items': latest_items
+    }) 
